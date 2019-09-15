fda7778804ba3ee83cad0464e78f94a17a071318
Aldo M <aldo.marukot@gmail.com>
2019-09-15 18:09:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,26 @@
-console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = []; 
-var cardOne;
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {		
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Try again.");
+		}	
 	}
-}
+};
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
 
