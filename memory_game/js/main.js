cb59dcba89871e01ee0750d10de45dbc50f9c065
Aldo M <aldo.marukot@gmail.com>
2019-09-16 09:47:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,43 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	suit: "hearts",
+	rank: "king",
+	image: "images/king-of-hearts.png"
+},
+{
+	suit: "hearts",
+	rank: "queen",
+	image: "images/queen-of-hearts.png"
+},
+{
+	suit: "diamonds",
+	rank: "queen",
+	image: "images/queen-of-diamonds.png"
+},
+{
+	suit: "diamonds",
+	rank: "king",
+	image: "images/king-of-diamonds.png"
+}];
 const cardsInPlay = []; 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {		
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			console.log("You found a match!");
 		} else {
-			console.log("Try again.");
+			alert("Try again.");
 		}	
 	}
 };
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].image);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 };
 flipCard(0);
 flipCard(2);
-
 
 
 
